4ac825258fc94bba07ef1ddd435fd07d8a168e97
Aime Aguado <aime.natalia.2406@gmail.com>
2025-11-24 16:48:12 -0300

    delete and edit users

--- home.js
+++ home.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   cargarUsers();
+  
+const close = document.getElementById("closeModal");
+  const modal = document.getElementById("myModal");
+
+
+close.addEventListener("click", () => {
+  modal.style.display = "none";
 });
+
+
+// Cerrar clickeando afuera
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
+});
+
+
 
 function cargarUsers() {
   fetch("https://back-nest-xi.vercel.app/users")
@@ -24,9 +42,11 @@ function mostrarTabla(usuarios) {
       <td>${user.lastname}</td>
       <td>${user.email}</td>
       <td>
-        <button class="bg-red-500 hover:bg-red-400 text-white px-3 py-1 rounded"
-                onclick="borrarUsuario('${user._id}')">
+        <button class="bg-red-500 hover:bg-red-400 text-white px-3 py-1 rounded" onclick="borrarUsuario('${user._id}')">
           Borrar
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-400 text-white px-3 py-1 rounded" onclick="mostrarModal('${user._id}')">
+          Editar
         </button>
       </td>
     `;
@@ -38,13 +58,67 @@ function mostrarTabla(usuarios) {
 async function borrarUsuario(id) {
 
   try {
-    const res = await fetch(`https://back-nest-xi.vercel.app/users/delete/${id}`, {
+    const res = await fetch(`https://back-nest-xi.vercel.app/users/${id}`, {
       method: "DELETE"
     });
 
-    alert("Usuario borrado correctamente");
+    if (res.ok){
+      alert("Usuario borrado correctamente");
+      // el usuario se borro
+      cargarUsers()
+    }
+
+    
 
   } catch (err) {
     console.error("Error borrando usuario:", err);
   }
+}
+
+async function mostrarModal(id) {
+  const modal = document.getElementById("myModal");
+  modal.style.display = "flex";
+  try{
+    const res = await fetch(`https://back-nest-xi.vercel.app/users/${id}`)
+  if (res.ok){
+    const usuario = await res.json();
+    console.log(usuario)
+    document.getElementById("email").value = usuario.email;
+    document.getElementById("nombre").value = usuario.name;
+    document.getElementById("apellido").value = usuario.lastname;
+    document.getElementById("usuarioId").value = id;
+  }
+  } catch (err) {
+    console.error("Error borrando usuario:", err);
+  }
+}
+
+async function editForm() {
+  try{ 
+    const id = document.getElementById("usuarioId").value
+    const name = document.getElementById("nombre").value;
+    const lastname = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    const modal = document.getElementById("myModal");
+    const usuarioEditado = {
+      name,
+      lastname,
+      email
+    }
+    const res = await fetch(`https://back-nest-xi.vercel.app/users/${id}`,  {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(usuarioEditado)
+        })
+        if (res.ok){
+          alert("Usuario editado")
+          modal.style.display = "none";
+          cargarUsers()
+        }
+  } catch (err) {
+    console.error("Error editando usuario:", err);
+  }
+  
 }
